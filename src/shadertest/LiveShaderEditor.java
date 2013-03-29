7113d7ba1e96de910ee10118e893dc55e9254a17
Maarten van Meersbergen <m.vanmeersbergen@esciencecenter.nl>
2013-03-29 13:16:08 +0100

    Initial import from svn.

---
+++ src/shadertest/LiveShaderEditor.java
@@ -0,0 +1,51 @@
+package shadertest;
+
+import javax.swing.JFrame;
+
+import nl.esciencecenter.esight.ESightNewtWindow;
+
+public class LiveShaderEditor {
+    public static void main(String[] arguments) {
+        String cmdlnfileName = null;
+        String path = "";
+
+        path = System.getProperty("user.dir");
+
+        final LiveShaderEditorGLEventListener window = new LiveShaderEditorGLEventListener(
+                LiveShaderEditorInputHandler.getInstance());
+        final LiveShaderEditorInterfacePanel panel = new LiveShaderEditorInterfacePanel(
+                window, path, cmdlnfileName);
+        // }
+        //
+        // public void makeWindow() {
+        new ESightNewtWindow(true, LiveShaderEditorInputHandler.getInstance(),
+                window, 1024, 768, "Shader Tester");
+
+        // Create the frame
+        final JFrame frame = new JFrame("- ST -");
+        frame.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent arg0) {
+                System.exit(0);
+            }
+        });
+
+        frame.setSize(240, 240);
+
+        frame.setResizable(false);
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    frame.getContentPane().add(panel);
+                } catch (final Exception e) {
+                    e.printStackTrace(System.err);
+                    System.exit(1);
+                }
+            }
+        });
+
+        frame.setVisible(true);
+    }
+}
